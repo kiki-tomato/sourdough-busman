ad2ea064cc3e87b9397ac374c8835e6a374b5dd8
kiki-tomato <jhyun.b@gmail.com>
2023-12-31 19:52:37 +0900

    test

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { useEffect, useRef } from "react";
+import "./App.css";
+
+function App() {
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    const { naver } = window;
+
+    if (!mapRef.current || !naver) {
+      const defaultLocation = new naver.maps.LatLng(35.1641776, 129.1181663);
+      const mapOptions = {
+        center: defaultLocation,
+        zoom: 10,
+      };
+      const map = new naver.maps.Map(mapRef, mapOptions);
+
+      new naver.maps.Marker({
+        position: defaultLocation,
+        map: map,
+      });
+    }
+  });
+
+  return <div ref={mapRef} className="map"></div>;
+}
+
+export default App;
