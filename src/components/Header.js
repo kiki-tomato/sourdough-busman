d58e8539ed57440a90cc1644251a2c90e1383144
kiki-tomato <jhyun.b@gmail.com>
2024-01-04 21:35:26 +0900

    minor updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,13 @@
 function Header() {
-  return <div className="header">Sourdough Busan</div>;
+  return (
+    <div className="header">
+      <div className="title">🥖 Sourdough Busan</div>
+      <div className="toggle-swtich">
+        <input id="switch" type="checkbox"></input>
+        <span class="slider round"></span>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
