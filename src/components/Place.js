a1ef90b27cf8be636889adebdbc76e3a280da71c
kiki-tomato <jhyun.b@gmail.com>
2024-01-15 21:44:13 +0900

    updating sidebar

--- src/components/Place.js
+++ src/components/Place.js
@@ -1,24 +1,37 @@
-import i18n from "../locales/i18n";
 import { useTranslation } from "react-i18next";
 
 function Place() {
-  console.log(i18n.language);
-
   const { t } = useTranslation();
-
   const bakeryData = t("bakeries", { returnObjects: true });
+  const places = [];
+  const d = new Date();
+  const currentDay = d.getDay();
+  const currentHours = d.getHours() + d.getMinutes() / 60;
+  let openOrNot;
+  let textColor;
 
-  const places = bakeryData.map((element, index) => {
-    return (
-      <div className="place" key={index}>
-        <h3>{t(element.name)}</h3>
-        <h5>{t(element.location)}</h5>
-        <h5>hours</h5>
+  for (let i = 0; i < bakeryData.length; i++) {
+    if (
+      bakeryData[i].hours[currentDay].open <= currentHours &&
+      currentHours < bakeryData[i].hours[currentDay].close
+    ) {
+      openOrNot = t("sideBar.openStatus.open", {
+        hour: bakeryData[i].hours[currentDay].close - 12,
+      });
+      textColor = "blue";
+    } else if (bakeryData[i].hours[currentDay].closed) {
+      openOrNot = t("sideBar.openStatus.closureDay");
+      textColor = "red";
+    }
+
+    places.push(
+      <div className="place" key={i}>
+        <h3>{t(`bakeries.${i}.name`)}</h3>
+        <h5>{t(`bakeries.${i}.location`)}</h5>
+        <h5 className={textColor}>{openOrNot}</h5>
       </div>
     );
-  });
-
-  console.log(places);
+  }
 
   return <div className="place-container">{places}</div>;
 }
